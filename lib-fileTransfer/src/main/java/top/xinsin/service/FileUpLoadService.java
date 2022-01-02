eb50b6f7eec36a7c1f32c7f4984c0c1b4ea29b4e
Wzp-2008 <minecraftwzpmc@163.com>
2022-01-02 12:41:22 +0800

    feat: add FileUpload function

--- lib-fileTransfer/src/main/java/top/xinsin/service/FileUpLoadService.java
+++ lib-fileTransfer/src/main/java/top/xinsin/service/FileUpLoadService.java
@@ -1,8 +1,7 @@
 package top.xinsin.service;
 
-import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Hex;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -13,16 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import top.xinsin.Utils.FileUtils;
 import top.xinsin.Utils.ResponseData;
 import top.xinsin.dao.FileUpLoadMapper;
 import top.xinsin.pojo.FileObject;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.MessageDigest;
+import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 
 /**
  * @Author xinxin
@@ -47,27 +46,36 @@ public class FileUpLoadService {
      */
 
     public ResponseData fileUpload(MultipartFile file) throws NoSuchAlgorithmException, IOException {
-        MessageDigest MD5 = MessageDigest.getInstance("MD5");
-        byte[] digest = MD5.digest(file.getBytes());
-        String md5 = String.valueOf(Hex.encodeHex(digest));//文件MD5
-        long size = file.getSize();
-        if (file.isEmpty()) {
+        if(file.isEmpty()){
             return new ResponseData(HttpStatus.BAD_REQUEST);
         }
-        String originalFilename = file.getOriginalFilename(); // 源文件名
-        FileObject fileObject = new FileObject(originalFilename, size, md5);
-        File dest = new File(FileSaveFolder + File.separator + md5);
-        try {
-            file.transferTo(dest);
-            fileUpLoadMapper.addFile(fileObject);
-            log.info("上传成功{}",fileObject);
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("md5",md5);
-            return new ResponseData(jsonObject,HttpStatus.OK);
-        } catch (IOException e) {
-            log.error(e.toString(), e);
+        log.info("starting saving");
+        String originalFilename = file.getOriginalFilename();//获取原始文件名
+        InputStream input =  file.getInputStream();//获取文件的InputStream
+        String savePath = FileSaveFolder + File.separator + originalFilename; //拼接文件保存路径
+        File file1 = new File(savePath);//获取文件对象
+        FileOutputStream out = new FileOutputStream(file1);//获取磁盘中的文件的OutputStream
+        IOUtils.copy(input,out);//使用IOUtils来复制
+        String md5 = FileUtils.calcMD5(file1);//计算磁盘中文件的md5
+        log.info("fileSaved,SavedName:{},md5:{}",savePath,md5);
+        input.close();//关闭输入流
+        out.close();//关闭输出流
+        log.info("closeIOStream");
+        log.info("fileRename to {}",md5);
+        String newSavePath = FileSaveFolder + File.separator + md5;//获取使用md5拼接的文件名
+        File newFile = new File(newSavePath);
+        if(newFile.exists()){
+            log.info("fileExists!");
+        }else{
+            boolean b = file1.renameTo(newFile);//将磁盘中的文件重命名
+            log.info("fileRenamed,Succeed:{}",b);
+            log.info("Sending to database");
+            FileObject fileObject = new FileObject(originalFilename,file.getSize(),md5);//新建FileObject
+            fileUpLoadMapper.addFile(fileObject);//上传数据库
+            log.info("Sanded");
+            log.info("fileUpload done");
         }
-        return new ResponseData(HttpStatus.EXPECTATION_FAILED);
+        return new ResponseData(md5);
     }
 
     public ResponseData getFileName(FileObject fileObject){
@@ -88,4 +96,5 @@ public class FileUpLoadService {
                 .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .body(new InputStreamResource(file.getInputStream()));
     }
+
 }
