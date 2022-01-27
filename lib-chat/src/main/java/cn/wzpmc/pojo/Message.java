e680de20886d616f16804e85a8ec4be8ec837b9f
33572 <18758003180@163.com>
2022-01-27 11:33:16 +0800

    refactor: refactor chat module using netty

---
+++ lib-chat/src/main/java/cn/wzpmc/pojo/Message.java
@@ -0,0 +1,43 @@
+package cn.wzpmc.pojo;
+
+import lombok.Data;
+import top.xinsin.enums.MessageTypes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author wzp
+ * @Date 2022/1/26
+ * @Version 1.0
+ */
+@Data
+public class Message {
+    int id;
+    String content;
+    String sender;
+    int recall;
+    String sendtime;
+    MessageTypes type;
+    public Message(String content, String sender,String type){
+        this.content = content;
+        this.sender = sender;
+        this.type = MessageTypes.valueOf(type);
+        this.sendtime = new SimpleDateFormat("y-M-d H:m:s").format(new Date());
+    }
+    public Message(String content, String sender,String type,String sendtime){
+        this.content = content;
+        this.sender = sender;
+        this.type = MessageTypes.valueOf(type);
+        this.sendtime = sendtime;
+    }
+    public Message(int id,String content,String sender,int recall,String type,String sendtime){
+        this.content = content;
+        this.sender = sender;
+        this.id = id;
+        this.recall = recall;
+        this.type = MessageTypes.valueOf(type);
+        this.sendtime = sendtime;
+    }
+}
+
