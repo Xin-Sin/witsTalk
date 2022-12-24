cb10e8ef6c68fa81fa0118549bec2d0e8e113a8b
xinsin <2890826955@qq.com>
2022-12-24 16:48:56 +0800

    ➕ Feat:add manage page,add dynamic routing

---
+++ web/src/views/admin/routeManage/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-button type="primary" round>添加新路由</el-button>
+    </el-header>
+    <el-main>
+
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
